f5a231b9c884d919d5db6a5f8ff83fb22e2a98db
bgraves <38325729+bgraves@users.noreply.github.com>
2022-07-07 14:17:10 -0400

    Somewhat functional first draft

---
+++ data.lua
@@ -0,0 +1,50 @@
+data:extend({
+    {
+        type = "custom-input",
+        name = "toggle-autorun",
+        key_sequence = "mouse-button-5",
+        consuming = "none"
+    },
+    {
+        type = "custom-input",
+        name = "move-up",
+        key_sequence = "",
+        linked_game_control = "move-up"
+    },
+    {
+        type = "custom-input",
+        name = "move-right",
+        key_sequence = "",
+        linked_game_control = "move-right"
+    },
+    {
+        type = "custom-input",
+        name = "move-down",
+        key_sequence = "",
+        linked_game_control = "move-down"
+    },
+    {
+        type = "custom-input",
+        name = "move-left",
+        key_sequence = "",
+        linked_game_control = "move-left"
+    },
+    {
+        type = "custom-input",
+        name = "mine",
+        key_sequence = "",
+        linked_game_control = "mine"
+    },
+    {
+        type = "custom-input",
+        name = "shoot-selected",
+        key_sequence = "",
+        linked_game_control = "shoot-selected"
+    },
+    {
+        type = "custom-input",
+        name = "shoot-enemy",
+        key_sequence = "",
+        linked_game_control = "shoot-enemy"
+    },
+})
